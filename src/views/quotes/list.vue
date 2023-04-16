4245a9417a1038026a258549120128bc88f8e717
Shakh7 <00011485wiut@gmail.com>
2023-04-17 04:24:02 +0500

    table searchs

--- src/views/quotes/list.vue
+++ src/views/quotes/list.vue
@@ -26,7 +26,45 @@
                         />
                       </span>
                     </button>
-                    <Dropdown2></Dropdown2>
+                    <div
+                            class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semobold w-200px"
+                            data-kt-menu="true"
+                    >
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3">
+                            <div class="menu-content fs-6 text-dark fw-bold px-3 py-4">
+                                Table Settings
+                            </div>
+                        </div>
+                        <!--end::Menu item-->
+
+                        <!--begin::Menu separator-->
+                        <div class="separator mb-3 opacity-75"></div>
+                        <!--end::Menu separator-->
+
+                        <!--begin::Menu item-->
+                        <div class="ps-5 p-3">
+                            <div class="form-check form-check-custom form-check-solid form-check-sm">
+                                <input v-model="table.search_server" class="form-check-input" type="checkbox" value=""
+                                       id="searchServerCheck">
+                                <label class="form-check-label" for="searchServerCheck">Search server</label>
+                            </div>
+                        </div>
+                        <!--end::Menu item-->
+
+                        <!--begin::Menu separator-->
+                        <div class="separator mt-3 opacity-75"></div>
+                        <!--end::Menu separator-->
+
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3">
+                            <div class="menu-content d-flex justify-content-between px-3 py-3">
+                                <a class="btn btn-transparent btn-sm px-4" href="#"> Reset </a>
+                                <a class="btn btn-primary btn-sm px-4" href="#"> Apply </a>
+                            </div>
+                        </div>
+                        <!--end::Menu item-->
+                    </div>
                 </div>
                 <div v-else>
                     <button class="btn btn-sm btn-primary" data-bs-toggle="modal"
@@ -40,31 +78,48 @@
 
         <!--begin::Body-->
         <div class="card-body py-3 px-6">
+
             <!--begin::Table container-->
             <div class="table-responsive">
                 <!--begin::Table-->
                 <table class="table align-middle gs-0 gy-4">
                     <!--begin::Table head-->
                     <thead>
-                    <tr class="fw-bold text-muted bg-light">
-                        <th style="max-width: 50px" class="ps-4 rounded-start">
+                    <tr class="fw-bold text-muted bg-light align-middle">
+                        <th style="max-width: 50px" class="ps-4 py-3 rounded-start">
                             <div class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox"
+                                       @change="pushAlToSelected"
+                                       :checked="this.selected_quotes.length === this.list.length"
+                                >
                             </div>
                         </th>
-                        <th>Customer</th>
-                        <th>Car</th>
-                        <th>Pick Up Address</th>
-                        <th>Drop Off Address</th>
-                        <th>PIck Up Date</th>
-                        <th>Actions</th>
+                        <th v-for="(item, index) in table_headers" :key="index" class="min-w-125px py-3">
+                            <select v-if="item.search_type === 'select'"
+                                    class="form-select form-select-sm form-select-solid"
+                                    :placeholder="'Search ' + item.text" v-model="item.search"
+                                    @change="searchChange()"
+                            >
+                                <option value="" selected>All</option>
+                                <option v-for="option in item.select_options" :key="option" :value="option.value">
+                                    {{ option.label }}
+                                </option>
+                            </select>
+                            <input v-else-if="item.search_type === 'date'"
+                                   class="form-control form-control-sm form-control-solid" type="date"
+                                   :placeholder="'Search ' + item.text" v-model="item.search"
+                                   @input="searchChange()"/>
+                            <input v-else class="form-control form-control-sm form-control-solid" type="text"
+                                   :placeholder="'Search ' + item.text" v-model="item.search"
+                                   @input="searchChange()"/>
+                        </th>
                     </tr>
                     </thead>
                     <!--end::Table head-->
 
                     <!--begin::Table body-->
-                    <tbody v-if="!is_loading">
-                    <template v-for="(item, index) in list" :key="index">
+                    <tbody v-if="!is_loading && table_data.length > 0">
+                    <template v-for="(item, index) in table_data" :key="index">
                         <tr>
                             <td class="ps-4">
                                 <div class="form-check form-check-custom form-check-solid">
@@ -78,7 +133,7 @@
                                     <a
                                             href="#"
                                             class="text-dark fw-bold text-hover-primary mb-1 fs-6"
-                                    >{{ item.customer.first_name + ' ' + item.customer.last_name }}</a
+                                    >{{ item.customer.full_name }}</a
                                     >
                                     <span
                                             class="text-muted fw-semobold text-muted d-block fs-7"
@@ -104,11 +159,11 @@
                                     <a
                                             href="#"
                                             class="text-dark fw-bold text-hover-primary mb-1 fs-6"
-                                    >{{ item.pick_up_address.city_name + ', ' + item.pick_up_address.state_code }}</a
+                                    >{{ item.destination.city_name + ', ' + item.destination.state_code }}</a
                                     >
                                     <span
                                             class="text-muted fw-semobold text-muted d-block fs-7"
-                                    >{{ item.pick_up_address.state_name + ', ' + item.pick_up_address.zip_code }}</span
+                                    >{{ item.destination.state_name + ', ' + item.destination.zip_code }}</span
                                     >
                                 </div>
                             </td>
@@ -117,22 +172,31 @@
                                     <a
                                             href="#"
                                             class="text-dark fw-bold text-hover-primary mb-1 fs-6"
-                                    >{{ item.drop_off_address.city_name + ', ' + item.drop_off_address.state_code }}</a
+                                    >{{ item.departure.city_name + ', ' + item.departure.state_code }}</a
                                     >
                                     <span
                                             class="text-muted fw-semobold text-muted d-block fs-7"
                                     >{{
-                                        item.drop_off_address.state_name + ', ' + item.drop_off_address.zip_code
+                                        item.departure.state_name + ', ' + item.departure.zip_code
                                         }}</span
                                     >
                                 </div>
+                            </td>
+                            <td class="text-center">
+                                <span class="badge" :class="{
+                                    'bg-success': item.is_lead === true,
+                                    'bg-danger': item.is_lead === false,
+                                }">
+                                    {{ item.is_lead ? 'Connected' : 'Not Connected' }}
+                                </span>
                             </td>
                             <td>
                                 {{ item.pick_up_date }}
                             </td>
                             <td>
                                 <button class="btn btn-sm btn-primary p-1 px-3 me-3" data-bs-toggle="modal"
-                                        data-bs-target="#share_quotes_model">
+                                        data-bs-target="#share_quotes_model"
+                                        @click="pushSingleQuote(item)">
                                     Share
                                 </button>
 
@@ -140,6 +204,24 @@
                             </td>
                         </tr>
                     </template>
+                    </tbody>
+
+                    <tbody v-else-if="!is_loading && this.table_headers.filter(i => i.search !== '').length > 0 && table_data.length === 0">
+                    <tr>
+                        <th :colspan="table_headers.length + 1" class="px-3 pt-10">
+                            <div class="mb-5">
+                                <span>We could not find any matching quotes for your queries !</span>
+                            </div>
+                            <div v-for="query in this.table_headers.filter(i => i.search !== '')" :key="query"
+                                 class="d-block mb-3">
+                                <span class="fw-semibold pe-3">{{ query.text }}:</span>
+                                <span>{{
+                                    query.value === 'is_lead' ? query.search === 'false' ? 'Not Connected' : 'Connected' : query.search
+                                    }}
+                                </span>
+                            </div>
+                        </th>
+                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -164,16 +246,111 @@ export default defineComponent({
     },
     data() {
         return {
+            timeout: 200,
             list: [],
             selected_quotes: [] as any,
             getAssetPath,
-            is_loading: true
+            is_loading: true,
+            table: {
+                search_server: true,
+                headers: [
+                    {
+                        text: "Customer",
+                        value: "customer",
+                        align: "start",
+                        width: "200px",
+                    },
+                    {
+                        text: "Car",
+                        value: "car",
+                        align: "start",
+                        width: "200px",
+                    },
+                    {
+                        text: "Pick Up Address",
+                        value: "pick_up_address",
+                        align: "start",
+                        width: "200px",
+                    },
+                    {
+                        text: "Drop Off Address",
+                        value: "drop_off_address",
+                        align: "start",
+                        width: "200px",
+                    },
+                    {
+                        text: "Status",
+                        value: "is_lead",
+                        align: "start",
+                        width: "200px",
+                        search_type: 'select',
+                        select_options: [
+                            {
+                                label: 'Connected',
+                                value: 'true'
+                            },
+                            {
+                                label: 'Not Connected',
+                                value: 'false'
+                            }
+                        ]
+                    },
+                    {
+                        text: "Pick Up Date",
+                        value: "pick_up_date",
+                        align: "start",
+                        width: "200px",
+                        search_type: 'date'
+                    },
+                    {
+                        text: "Actions",
+                        value: "actions",
+                        sortable: false,
+                        align: "start",
+                        width: "200px",
+                    }
+                ]
+            }
         };
+    },
+    computed: {
+        table_headers() {
+            return this.table.headers.map((item: any) => {
+                return {
+                    ...item,
+                    search: '',
+                };
+            });
+        },
+        table_data() {
+            if (this.table.search_server) {
+                return this.list
+            } else {
+                let searched_fields = this.table_headers.filter(i => i.search !== '').map(i => {
+                    return {
+                        key: i.value,
+                        value: i.search
+                    }
+                })
+                return this.list
+            }
+        }
     },
     methods: {
         async getQuotes() {
-            this.is_loading = true
             let response = await axios.get("http://68.183.109.5:3000/api/quotes");
+            if (response.status === 200) {
+                this.is_loading = false
+                this.list = response.data.results;
+            }
+        },
+        async searchQuotesServer(searched_fields: any []) {
+            let url = `http://68.183.109.5:3000/api/quotes`
+            searched_fields.forEach((item, index) => {
+                index === 0 ? url += `?${item.key}=${item.value}` : url += `&${item.key}=${item.value}`
+            })
+            console.log(url)
+            let response = await axios.get(url);
             if (response.status === 200) {
                 this.is_loading = false
                 this.list = response.data.results;
@@ -187,7 +364,32 @@ export default defineComponent({
             }
         },
         pushSingleQuote(quote) {
-
+            if (this.selected_quotes.map(i => i.id).includes(quote.id)) {
+                return
+            } else {
+                this.selected_quotes.push(quote)
+            }
+        },
+        pushAlToSelected() {
+            if (this.selected_quotes.length === this.list.length) {
+                this.selected_quotes = []
+            } else {
+                this.selected_quotes = this.list
+            }
+        },
+        searchChange() {
+            if (!this.table.search_server) return
+            let searched_fields = this.table_headers.filter(i => i.search !== '').map(i => {
+                return {
+                    key: i.value,
+                    value: i.search
+                }
+            })
+            console.log(searched_fields)
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => {
+                this.searchQuotesServer(searched_fields)
+            }, 500); // 500ms delay
         }
     },
     mounted() {
